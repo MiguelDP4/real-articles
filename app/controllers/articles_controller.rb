420a65d74ce46572e9990fcf08606c5fd58df893
migueldp4 <migueldp4@gmail.com>
2020-05-13 17:07:59 -0600

    add article form, user now can post articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,17 @@ class ArticlesController < ApplicationController
   before_action :require_login
   before_action :logged_in_user, only: [:create, :destroy, :edit]
 
+  def new
+
+  end
+
   def create
     @article = current_user.articles.build(article_params)
     if @article.save
       flash[:success] = "Your article was published."
       redirect_to @article
     else
-      flash[:danger] = "Something went wrong. Try again later."
+      flash[:danger] = "Something went wrong. Try again later. #{p article_params}"
       redirect_to root_path
     end
   end
@@ -17,11 +21,26 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      flash[:danger] = "Something went wrong. Try again later."
+      redirect_to root_path
+    end
+  end
+
   private
 
   def article_params
-    params.require(:article).permit(:title, :text, :image)
+    params.permit(:title, :text, :image)
   end
+
   def logged_in_user
     unless user_signed_in?
       flash[:danger] = "Log in first to be able to create articles."
