7ae53b480c9220a45c2b68f542d87e36302275ee
migueldp4 <migueldp4@gmail.com>
2020-05-13 09:56:55 -0600

    Configure app to require login for all pages except registration and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+  private
+
+  def require_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
