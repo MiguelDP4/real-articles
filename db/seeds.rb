10fa3c9a1fc3fe32ed05bb088575487fdac1a81c
migueldp4 <migueldp4@gmail.com>
2020-05-15 11:42:13 -0600

    Add records to seed file to populate categories on database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Miscelaneus", priority: 5)
+Category.create(name: "Technology", priority: 1)
+Category.create(name: "World", priority: 3)
+Category.create(name: "Real", priority: 2)
+Category.create(name: "Sports", priority: 4)
