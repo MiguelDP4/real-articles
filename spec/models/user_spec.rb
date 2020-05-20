71c4c6b08746a3a8507d2592a04267be1f79f802
migueldp4 <migueldp4@gmail.com>
2020-05-20 11:29:10 -0600

    Add tests for associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,9 @@ RSpec.describe User, type: :model do
       user = User.new(name: 'name', email: 'email@email.com', password: 'asdfasdf').save
       expect(user).to eq(true)
     end
+  end
 
-    it 'ensures user names are unique' do
-      user = User.new(name: 'name', email: 'email@email.com', password: 'asdfasdf').save
-      user2 = User.new(name: 'name', email: 'email2@email.com', password: 'asdfasdf').save
-      expect(user).to eq(true)
-      expect(user2).to eq(false)
-    end
-
+  context 'validations' do
     it 'ensures user emails are unique' do
       user = User.new(name: 'name', email: 'email@email.com', password: 'asdfasdf').save
       user2 = User.new(name: 'name2', email: 'email@email.com', password: 'asdfasdf').save
@@ -21,9 +16,47 @@ RSpec.describe User, type: :model do
       expect(user2).to eq(false)
     end
 
-    it 'ensures users can be created' do
+    it 'ensures user names are unique' do
       user = User.new(name: 'name', email: 'email@email.com', password: 'asdfasdf').save
+      user2 = User.new(name: 'name', email: 'email2@email.com', password: 'asdfasdf').save
       expect(user).to eq(true)
+      expect(user2).to eq(false)
+    end
+  end
+
+  context 'public methods' do
+    it 'tests if user hasnt voted for article' do
+      user = User.create!(name: 'name', email: 'email@email.com', password: 'asdfasdf')
+      article = user.articles.build(text: "text of article", title: "title of article", image: "")
+      article.save
+      expect(user.voted?(article)).to eq(false)
+    end
+
+    it 'tests if user voted for article' do
+      user = User.create!(name: 'name', email: 'email@email.com', password: 'asdfasdf')
+      article = user.articles.build(text: "text of article", title: "title of article", image: "")
+      article.save
+      vote = user.votes.build(article_id: article.id)
+      vote.save
+      expect(user.voted?(article)).to eq(true)
+    end
+  end
+
+  context 'associations' do
+    it 'tests association between user and article' do
+      user = User.create!(name: 'name', email: 'email@email.com', password: 'asdfasdf')
+      article = user.articles.build(text: "text of article", title: "title of article", image: "")
+      article.save
+      expect(user.articles.first).to eq(article)
+    end
+
+    it 'tests association between user and votes' do
+      user = User.create!(name: 'name', email: 'email@email.com', password: 'asdfasdf')
+      article = user.articles.build(text: "text of article", title: "title of article", image: "")
+      article.save
+      vote = user.votes.build(article_id: article.id)
+      vote.save
+      expect(user.votes.first).to eq(vote)
     end
   end
 end
