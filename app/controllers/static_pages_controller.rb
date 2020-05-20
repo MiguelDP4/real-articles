ba8def058d9914f233f72b6de9621738639c15d3
migueldp4 <migueldp4@gmail.com>
2020-05-20 08:54:43 -0600

    Remove database reference from view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @articles = Article.all
+    @categories = Category.all.sort_by(&:priority)
     string_message = 'Looks like nobody has published anything yet. Be the first one to create an article!'
     flash.now[:warning] = string_message if @articles.none?
   end
